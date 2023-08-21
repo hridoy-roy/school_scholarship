b8e86db26f728324e4180cc42bacb427f2e1c148
Hridoy Roy <41943565+hridoy-roy@users.noreply.github.com>
2023-08-21 21:30:44 +0600

    dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,8 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Blog;
+use App\Models\Exam;
+use App\Models\Member;
+use App\Models\Slider;
+use App\Models\Student;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Gallery;
+use App\Models\Institute;
+use App\Models\StudentClass;
 
 class DashboardController extends Controller
 {
@@ -12,7 +20,18 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $data = [];
+        $data = [
+            'studentInThisYear' => Student::whereYear('created_at', '=', date('Y'))->count(),
+            'studentInThisYearPay' => Student::whereYear('created_at', '=', date('Y'))->where('payment_status', 'paid')->count(),
+            'totalStudent' => Student::count(),
+            'totalExam' => Exam::count(),
+            'totalMember' => Member::count(),
+            'totalBlog' => Blog::count(),
+            'totalSlider' => Slider::count(),
+            'totalGallery' => Gallery::count(),
+            'totalSchool' => Institute::count(),
+            'totalClass' => StudentClass::count(),
+        ];
         return view('admin.content.dashboard.dashboard', $data);
     }
 }
