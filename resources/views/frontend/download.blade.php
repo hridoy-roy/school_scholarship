bee676e29c3e6d93ae2a4f9d12d63ca1b4af013d
Banik Parthib <litcoder.banik@gmail.com>
2023-08-25 00:34:41 +0600

    registration download print work

--- resources/views/frontend/download.blade.php
+++ resources/views/frontend/download.blade.php
@@ -135,10 +135,10 @@
                 <div class="section-one-info">
 
                     <div class="info-line">
-                        <span class="label">School:</span>{{ $student['institute']['name'] }}
+                        <span class="label">School:</span>{{ $student['school_madrasa']}}
                     </div>
                     <div class="info-line">
-                        <span class="label">Gender:</span>{{ $student['name_bn'] }} Male
+                        <span class="label">Gender:</span>{{ $student['student_type'] }}
                     </div>
                 </div>
 
@@ -150,93 +150,96 @@
             <div class="section-two">
                 <div class="section-two-title">Student Information</div>
                 <div class="info-line">
-                    <span class="label">Name(English):</span>{{ $student['name_bn'] }} Mohammad Asif Uddin
+                    <span class="label">Name(English):</span>{{ $student['name_en'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Area:</span>{{ $student['name_bn'] }} Chittagong
+                    <span class="label">Area:</span>{{ $student['area'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">School Name:</span>{{ $student['name_bn'] }} Chittagong Govt. High School
+                    <span class="label">School Name:</span>{{ $student['institute']['name'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Class:</span>{{ $student['name_bn'] }} Eight
+                    <span class="label">Class:</span>{{ $student['student_class']['name'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Class Roll:</span>{{ $student['name_bn'] }} 12
+                    <span class="label">Class Roll:</span>{{ $student['class_roll'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Section:</span>{{ $student['name_bn'] }} Meghna
+                    <span class="label">Subject:</span>{{ $student['class_division'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Date Of Birth:</span>{{ $student['name_bn'] }} 01/01/2010
+                    <span class="label">Section:</span>{{ $student['class_section'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Blood Group:</span>{{ $student['name_bn'] }} AB+
+                    <span class="label">Date Of Birth:</span>{{ $student['dob'] }}
+                </div>
+                <div class="info-line">
+                    <span class="label">Blood Group:</span>{{ $student['blood_group'] }}
                 </div>
 
             </div>
             <div class="section-three">
                 <div class="section-three-title">Parents Information</div>
                 <div class="info-line">
-                    <span class="label">Father's Name:</span>{{ $student['name_bn'] }} Moinuddin Hamid
+                    <span class="label">Father's Name:</span>{{ $student['father_name_en'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Profession:</span>{{ $student['name_bn'] }} Businessman
+                    <span class="label">Profession:</span>{{ $student['father_occupation'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Mother's Name:</span>{{ $student['name_bn'] }} Halima Begum
+                    <span class="label">Mother's Name:</span>{{ $student['mother_name_en'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Profession:</span>{{ $student['name_bn'] }} Housewife
+                    <span class="label">Profession:</span>{{ $student['mother_occupation'] }}
                 </div>
             </div>
             <div class="section-fourth">
                 <div class="section-two-title">Present Address</div>
                 <div class="info-line">
-                    <span class="label">Home Address:</span>{{ $student['name_bn'] }} GEC, Naziabad, Chittagong
+                    <span class="label">Home Address:</span>{{ $student['present_address'] }}
                 </div>
             </div>
 
             <div class="section-fifth">
                 <div class="section-fifth-title">Parmanent Address</div>
                 <div class="info-line">
-                    <span class="label">Division:</span>{{ $student['name_bn'] }} Chittagong
+                    <span class="label">Division:</span>{{ $student['permanent_address_district'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Village Name:</span>{{ $student['name_bn'] }} Chittagong
+                    <span class="label">Village Name:</span>{{ $student['permanent_address_village'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Thane:</span>{{ $student['name_bn'] }} Chittagong
+                    <span class="label">Thane:</span>{{ $student['permanent_address_thana'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Post Code:</span>{{ $student['name_bn'] }} 4225
+                    <span class="label">Post Code:</span>{{ $student['permanent_address_post_office'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Home Address:</span>{{ $student['name_bn'] }} GEC, Naziabad, Chittagong
+                    <span class="label">Home Address:</span>{{ $student['permanent_address'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Mobile Number:</span>{{ $student['name_bn'] }} +8801340282233
+                    <span class="label">Mobile Number:</span>{{ $student['mobile'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Email Address:</span>{{ $student['name_bn'] }} xyab@gmail.com
+                    <span class="label">Email Address:</span>{{ $student['email'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Facebook:</span>{{ $student['name_bn'] }} Mohammad Asif Uddin
+                    <span class="label">Facebook:</span>{{ $student['facebook'] }}
                 </div>
             </div>
             <div class="section-sixth">
                 <div class="section-sixth-title">Absence Of Guardian Information</div>
                 <div class="info-line">
-                    <span class="label">Name Of Guardian:</span>{{ $student['name_bn'] }} Moinuddin Hamid
+                    <span class="label">Name Of Guardian:</span>{{ $student['absent_of_parent_name'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Relationship:</span>{{ $student['name_bn'] }} Father
+                    <span class="label">Relationship:</span>{{ $student['absent_of_parent_relation'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Occupation:</span>{{ $student['name_bn'] }} Businessman
+                    <span class="label">Occupation:</span>{{ $student['absent_of_parent_occupation'] }}
                 </div>
                 <div class="info-line">
-                    <span class="label">Income:</span>{{ $student['name_bn'] }} 300000
+                    <span class="label">Income:</span>{{ $student['absent_of_parent_annual_earning'] }}
                 </div>
             </div>
 
