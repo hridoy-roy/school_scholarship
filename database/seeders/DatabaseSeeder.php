7df6c3ed21de958c35bb138915f1d69c53ae7fca
Hridoy Roy <41943565+hridoy-roy@users.noreply.github.com>
2023-08-29 23:12:38 +0600

    Seed add

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,7 +29,8 @@ class DatabaseSeeder extends Seeder
             'name' => 'Dhaka School'
         ]);
         StudentClass::create([
-            'name' => 'One'
+            'name' => 'One',
+            'order_by' => 1
         ]);
     }
 }
