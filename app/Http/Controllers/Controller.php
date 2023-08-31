ea53004ae94a014793f085eeaca23bc992ff4331
Hridoy Roy <41943565+hridoy-roy@users.noreply.github.com>
2023-08-31 20:08:47 +0600

    Error

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,27 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CreateManu;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-use App\Models\CreateManu;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
     public function __construct()
-
     {
-
         $menus = CreateManu::where('status', 'Enabled')->get();
-
         view()->share('menus', $menus);
-        
-
-        
     }
 }
-
-
